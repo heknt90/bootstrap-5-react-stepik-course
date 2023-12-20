e99e84d33d7695a08f7c3fde0683b06545cff37f
Nikita Efremov <heknt90@mail.ru>
2023-12-20 16:38:48 +0300

    removed bootstrap.js import from index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import './index.css';
 import App from './App';
